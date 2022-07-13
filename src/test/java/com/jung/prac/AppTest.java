77e8c14d5d33215968554f7663a540ca99638952
ap3334 <chwil2989@naver.com>
2022-07-13 14:22:04 +0900

    등록 테스트 작성

--- src/test/java/com/jung/prac/AppTest.java
+++ src/test/java/com/jung/prac/AppTest.java
@@ -2,10 +2,30 @@ package com.jung.prac;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    @Test
+    public void 등록_테스트() {
 
+        List<WiseSaying> wiseSayingList = new ArrayList<>();
+        WiseSaying wiseSaying = new WiseSaying();
+
+        wiseSaying.setContent("명언");
+        wiseSaying.setAuthor("작가");
+
+        wiseSayingList.add(wiseSaying);
+
+        assertEquals(1, wiseSayingList.size());
+        assertEquals("명언", wiseSayingList.get(0).getContent());
+        assertEquals("작가", wiseSayingList.get(0).getAuthor());
+
+
+
+    }
 
 }
